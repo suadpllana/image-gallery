cffc4050609bfd55b1d534bec652306246ccc3c7
oltapllana <oltapllana@gmail.com>
2025-01-13 17:31:30 +0100

    first commit

--- src/Images.jsx
+++ src/Images.jsx
@@ -1,67 +1,83 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const Images = () => {
   const inputRef = useRef(null);
   const [imagesData, setImagesData] = useState([]);
-  const [loading , setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1); 
+  const [bigImage, setBigImage] = useState();
   const accessKey = import.meta.env.VITE_API_KEY;
 
-  async function images(newPage) {
-    setLoading(true)
-    try{
-        if(inputRef.current.value === ``){
-            alert("Please fill the field")
-            return
-          }
-          const url = `https://api.unsplash.com/search/photos?page=${newPage}&query=${inputRef.current.value}&client_id=${accessKey}`;
-          const response = await fetch(url);
-          const data = await response.json();
-          if (!response.ok) {
-           
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          
-          if (newPage === 1) {
-           
-            setImagesData(data.results);
-          } else {
-            
-            setImagesData((prevImages) => [...prevImages, ...data.results]);
-          }
-          setLoading(false)
+  
+  useEffect(() => {
+    if (imagesData.length > 0 && !bigImage) {
+      setBigImage(imagesData[0].urls.small);
     }
-   catch(error){
-    alert("Something went wrong")
-    return
-   }
+  }, [imagesData, bigImage]);
+
+  async function images(newPage) {
+    setLoading(true);
+    try {
+        if(inputRef.current.value === "") {
+            alert("Please fill the field");
+            return;
+        }
+        const url = `https://api.unsplash.com/search/photos?page=${newPage}&query=${inputRef.current.value}&client_id=${accessKey}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        if (newPage === 1) {
+          
+          setBigImage(undefined);
+          setImagesData(data.results);
+        } else {
+          setImagesData((prevImages) => [...prevImages, ...data.results]);
+        }
+        setLoading(false);
+    } catch (error) {
+        alert("Something went wrong");
+    }
   }
 
   function showMore() {
     setPage(prev => prev + 1); 
     images(page);  
   }
-  function enter(e){
-    if(e.key === "Enter"){
-        images(page)
+
+  function enter(e) {
+    if(e.key === "Enter") {
+        images(1); 
     }
- 
   }
 
   return (
     <div>
       <h1>Search for any images</h1>
       <input onKeyDown={(e) => enter(e)} ref={inputRef} type="text" />
-      <button onClick={() => images(1)}>Search</button>
+      <button onClick={() => images(1)}>Search</button><br />
       {imagesData.length > 0 && (
-        <>
-          <div className="container">
-            {imagesData.map((image, index) => (
-              <img key={index} src={image.urls.small} alt="" />
-            ))}
-          </div>
-        </>
-      ) }
+        <img className="big-image" src={bigImage} alt="" />
+      )}
+
+      {imagesData.length > 0 && (
+        <div className="container">
+          {imagesData.map((image, index) => (
+            <img
+              style={{
+                border: bigImage === image.urls.small ? '3px solid purple' : 'none'
+              }}
+              key={index}
+              src={image.urls.small}
+              onClick={() => setBigImage(image.urls.small)}
+              alt=""
+            />
+          ))}
+        </div>
+      )}
+      
       {loading && <p>Loading...</p>}
 
       {imagesData.length >= 10 && 
@@ -71,6 +87,6 @@ const Images = () => {
       }
     </div>
   );
-};
+}
 
 export default Images;
